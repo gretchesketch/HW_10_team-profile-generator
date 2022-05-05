899d448a772f03950093f15d833917978ed06075
gretchesketch <gretchenjensen92@gmail.com>
2022-05-05 13:54:43 -0400

    got alot of the index js file finished still peacing it together no tests yet and working on the read me

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # HW_10_team-profile-generator
+
+Table CONTENTS
+---------------------
+
+ * Introduction
+ * Requirements
+ * Tech
+ * Installation
+ * Configuration
+ * Troubleshooting
+ * FAQ
+ * Contributers
+
+
+
+ INTRODUCTION
+----------------------
+
+An app that promts a user in the command line to input various employee information that will eventually be displayed in an html document and a brower.
+
+ * Live URL:
+
+ * Repo URL:
--- index.js
+++ index.js
@@ -1,1 +1,166 @@
-var inquirer = require("inquirer");
+// required packages
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+// Employee template based on these below.
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+
+
+// This array fills in with employee data.
+// used const so it wont change
+const teamMembers = [];
+// let because it will change values
+let manager;
+// let because it will change values
+let teamTitle;
+
+
+function managerData() {
+    inquirer.prompt([
+        {   // Fill html with teamName.
+            type: "input",
+            message: "What is the name of this team/project?",
+            name: "teamTitle"
+        },
+        {   // There is only 1 manager for a team.
+            type: "input",
+            message: "Who is the manager of this project?",
+            name: "managerName"
+        },
+        {   // Employee ID.
+            type: "input",
+            message: "What is the manager's ID?",
+            name: "managerID"
+        },
+        {   // Employee Email.
+            type: "input",
+            message: "What is the manager's email?",
+            name: "managerEmail"
+        },
+      
+        ]).then(managerAnswers => {
+            manager = new Manager(managerAnswers.managerName, managerAnswers.managerID, managerAnswers.managerEmail, managerAnswers.officeNumber);
+            teamTitle = managerAnswers.teamTitle;
+            console.log("Next id elligible employee information")
+            employeeData();
+        });
+}
+
+function employeeData() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What is this employee's role?",
+            name: "employeeRole",
+            choices: ["Intern", "Engineer"]
+        },
+
+       
+        {
+            type: "input",
+            message: "What is the employee's name?",
+            name: "employeeName"
+        },
+        {
+            type: "input",
+            message: "What is the employee's id?",
+            name: "employeeId"
+        },
+        {
+            type: "input",
+            message: "What is the employee's email?",
+            name: "employeeEmail"
+        },
+        {
+            type: "input",
+            message: "What is the Engineer's Github?",
+            name: "github",
+            when: (userInput) => userInput.employeeRole === "Engineer"
+        },
+        {
+            type: "input",
+            message: "What's the Intern's school?",
+            name: "school",
+            when: (userInput) => userInput.employeeRole === "Intern"
+        },
+         // if yes, go back again. If no, renderHTML
+        {
+            type: "confirm",
+            name: "newEmployee",
+            message: "Would you like to add another team member?"
+        }
+    ]).then(answers => {
+       
+        if (answers.employeeRole === "Intern") {
+            const employee = new Intern(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.school);
+            teamMembers.push(employee);
+        } else if (answers.employeeRole === "Engineer") {
+            // A different way of pushing the info into teamMembers array.
+            teamMembers.push(new Engineer(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.github));
+        }
+        if (answers.newEmployee === true) {
+            employeeData();
+        } else {
+           
+
+            var main = fs.readFileSync('./templates/main.html', 'utf8');
+            // The slashes and g => regular expressions (regex)
+            // This allows the replace function to replace all occurances of teamTitle.
+            // If I just did '{{teamTitle}}' then it only replaces the first instance.
+            main = main.replace(/{{teamTitle}}/g, teamTitle);
+
+            // Loop through the employees and print out all of their cards without replacing the previous one.
+            var managerCard = fs.readFileSync('./templates/Manager.html', 'utf8');
+            managerCard = managerCard.replace('{{name}}', manager.getName());
+            managerCard = managerCard.replace('{{role}}', manager.getRole());
+            managerCard = managerCard.replace('{{id}}', manager.getId());
+            managerCard = managerCard.replace('{{email}}', manager.getEmail());
+            managerCard = managerCard.replace('{{officeNumber}}', manager.getOfficeNumber());
+
+            
+            // declaring cards to hold the value of managerCard
+            var cards = managerCard; 
+            // for loop to continute to create cards as long as the input has been submitted.
+            //stops when it reaches the length limit of the user input that went into the blank array
+            for (var i = 0; i < teamMembers.length; i++) {
+                var employee = teamMembers[i];
+                // Cards adds and then equals every new employee card info.
+                cards += renderEmployee(employee);
+            }
+
+            // Adds cards to main.html and outputs to team.html.
+            main = main.replace('{{cards}}', cards);
+
+            fs.writeFileSync('./output/team.html', main);
+
+            // checking that the html has been generated
+            console.log("The team.html has been generated in output");
+        }
+    });
+}
+
+// renderEmployee function that is called above using if and if else
+
+function renderEmployee(employee) {
+    if (employee.getRole() === "Intern") {
+        var internCard = fs.readFileSync('./templates/Intern.html', 'utf8');
+        internCard = internCard.replace('{{name}}', employee.getName());
+        internCard = internCard.replace('{{role}}', employee.getRole());
+        internCard = internCard.replace('{{id}}', employee.getId());
+        internCard = internCard.replace('{{email}}', employee.getEmail());
+        internCard = internCard.replace('{{school}}', employee.getSchool());
+        return internCard;
+    } else if (employee.getRole() === "Engineer") {
+        var engineerCard = fs.readFileSync('./templates/Engineer.html', 'utf8');
+        engineerCard = engineerCard.replace('{{name}}', employee.getName());
+        engineerCard = engineerCard.replace('{{role}}', employee.getRole());
+        engineerCard = engineerCard.replace('{{id}}', employee.getId());
+        engineerCard = engineerCard.replace('{{email}}', employee.getEmail());
+        engineerCard = engineerCard.replace('{{github}}', employee.getGithub());
+        return engineerCard;
+    }
+}
+
+managerData();
